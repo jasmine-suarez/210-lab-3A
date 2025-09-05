972a12cf7f0e0002340fd9f5baea127b6d7fce9a
Jasmine <jazzyysuarez@gmail.com>
2025-09-04 22:45:33 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+struct Restaurant
+{
+    string name;
+    string address;
+    int rating;
+    double avgPrice;
+    bool isOpen;
+};
+
+int main()
+{
+    return 0;
+}
